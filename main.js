0fc022d5318a1a9be9509f1246c6736b8d93bce1
idleRain <gold.experience@foxmail.com>
2022-06-22 11:48:55 +0800

    init

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 
+// 封装的网络请求
+import './utils/http'
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+
 const app = new Vue({
-  ...App
+	...App
 })
 app.$mount()
